0e8c093fae3be1aee41dc114e8dc1d6b60b525bf
DN8630 <dayanayak30@gmail.com>
2020-10-28 22:44:51 -0400

    Requesting the days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,10 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment"
 import Appointment from "components/Appointment";
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 const appointments = [
   {
     id: 1,
@@ -70,7 +54,17 @@ const appointments = [
 ];
 export default function Application(props) {
   const [currentDay,setDay] = useState("Monday");
-
+  const [days,setDays] = useState([]);
+  
+  useEffect(() => {
+    axios.get('/api/days')
+    .then(response => {
+      setDays(response.data);
+    })
+    .catch(error => {
+      console.log(error.message);
+    })
+  },[])
   return (
     <main className="layout">
       <section className="sidebar">
